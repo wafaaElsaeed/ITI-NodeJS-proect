6d897d812f2d16be946bc686547eb2f40af6a12c
wafaaElsaeed <77937661+wafaaElsaeed@users.noreply.github.com>
2021-01-25 04:13:07 +0200

    l

--- routes/blog.js
+++ routes/blog.js
@@ -8,7 +8,7 @@ const multer = require('multer') ;
 const path = require('path');
 const storage = multer.diskStorage({
     destination: function(req ,file ,cb){
-      cb(null, 'images/')
+      cb(null, 'images')
     },
     filename: function(req ,file ,cb){
       cb(null, file.originalname + '-' + Date.now() + path.extname(file.originalname));
